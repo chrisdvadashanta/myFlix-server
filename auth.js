97f42a7b18fb9bbba34f92a8ed7c5b6c925b8af7
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-06-02 11:18:28 +0200

    solved case sensetive issues with username and password

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ require('./passport'); // Your local passport file
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username, // This is the username you’re encoding in the JWT
+    subject: user.username, // This is the username you’re encoding in the JWT
     expiresIn: '7d', // This specifies that the token will expire in 7 days
     algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
   });
@@ -20,6 +20,7 @@ module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
+        console.log("error",error);
         return res.status(400).json({
           message: 'Something is not right',
           user: user
@@ -30,7 +31,7 @@ module.exports = (router) => {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token }); //shorthand for: res.json({ user: user, token: token })
+        return res.json({ user, token });
       });
     })(req, res);
   });
--- passport.js
+++ passport.js
@@ -8,11 +8,11 @@ let Users = Models.User,
   ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password'
+  usernameField: 'username',
+  passwordField: 'password'
 }, (username, password, callback) => {
   console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
+  Users.findOne({ username: username }, (error, user) => {
     if (error) {
       console.log(error);
       return callback(error);
@@ -20,7 +20,7 @@ passport.use(new LocalStrategy({
 
     if (!user) {
       console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      return callback(null, false, { message: 'Incorrect username or password.' });
     }
 
     console.log('finished');
@@ -28,18 +28,15 @@ passport.use(new LocalStrategy({
   });
 }));
 
-
 passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-  }, (jwtPayload, callback) => {
-    return Users.findById(jwtPayload._id)
-      .then((user) => {
-        return callback(null, user);
-      })
-      .catch((error) => {
-        return callback(error)
-      });
-  }));
-
-
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
+}));
