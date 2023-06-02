12c81502af0c4c911587df6e79253e87a6590f3c
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-06-02 16:16:42 +0200

    changed the User variabel for the model

--- passport.js
+++ passport.js
@@ -3,30 +3,30 @@ const passport = require('passport'),
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
 
-let Users = Models.User,
+let Users = Models.Users,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
-  usernameField: 'username',
-  passwordField: 'password'
-}, (username, password, callback) => {
-  console.log(username + '  ' + password);
-  Users.findOne({ username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
-
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, { message: 'Incorrect username or password.' });
-    }
-
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+  passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+  }, (username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({ username: username })
+      .then((user) => {
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, { message: 'Incorrect username or password.' });
+        }
+  
+        console.log('finished');
+        return callback(null, user);
+      })
+      .catch((error) => {
+        return callback(error);
+      });
+  }));
+  
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
