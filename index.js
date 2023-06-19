9298d51a0ae93a8c4b97cb555adf3d0855e78861
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-06-19 09:29:08 +0200

    connection_URi path change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const Movies = Models.Movie;
 const Users = Models.Users;
 
 // let CONNECTION_URI = "mongodb+srv://christophkeller0:12345@myflix.eh4zhje.mongodb.net/"
-mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 ////////////Local testing Code /////////////
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
