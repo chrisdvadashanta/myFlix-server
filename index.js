01eab2e7695460d0321a8709477f1870e128c208
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-06-19 08:42:27 +0200

    changing database connection URL to process.env

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ const Models = require('./models');
 const Movies = Models.Movie;
 const Users = Models.Users;
 
+require('dotenv').config(); // Load .env file
+
+
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-let CONNECTION_URI = "mongodb+srv://christophkeller0:12345@myflix.eh4zhje.mongodb.net/"
+let CONNECTION_URI = process.env.dataBase
 mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 /////// EXPRESS ///////
 const app = express();
